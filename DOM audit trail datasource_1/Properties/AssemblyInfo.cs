d882376cf7decbafe82fcaf31ad8338312f4bdf3
martijnskyline <109655483+martijnskyline@users.noreply.github.com>
2023-05-04 17:32:03 +0200

    Add project files.

---
+++ DOM audit trail datasource_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DOM audit trail datasource_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DOM audit trail datasource_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E5D48259-B098-4FED-8E4C-ABB9C05138D0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# DOM audit trail datasource
